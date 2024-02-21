6b2cda55354707a589877029d858c96b4ce8d77d
LupeF <46634202+LupeF@users.noreply.github.com>
2024-02-21 11:30:59 -0800

    add styles and transition effects to nav buttons

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const hamburgerContainer = document.getElementById('hamburger-cont');
+const xContainer = document.getElementById('close-cont');
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const listItems = document.getElementById('list-item-cont');
+hamburgerMenu.addEventListener('click', (e)=>{
+    if(xContainer.style.zIndex !== -1){
+        listItems.classList.toggle('menu-slide');
+        xContainer.style.zIndex = 2;
+    }
+})
+xContainer.addEventListener('click', (e)=>{
+    xContainer.style.zIndex = -1
+    listItems.classList.toggle('menu-slide');
+})
